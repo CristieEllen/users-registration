3d5a161b6496cccf277830a7021eca295e776a8e
Cristie Ferreira <cristie_lopes_ferreira@hotmail.com>
2022-07-11 15:41:14 -0300

    Remoção do arquivo

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_customers(name, document_number, email, cellphone, customer_type) VALUES('Ana', '12345678901', 'ana@gmail.com', '12912342345', 'PF' );
-INSERT INTO tb_customers(name, document_number, email, cellphone, customer_type) VALUES('Maria', '09876543212', 'maria@gmail.com', '12998765432', 'PF');
