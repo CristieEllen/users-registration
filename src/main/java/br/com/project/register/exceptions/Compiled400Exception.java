c95024f97f20ffe77b60f604ae790c464af870c8
Cristie Ferreira <cristie_lopes_ferreira@hotmail.com>
2022-07-14 14:57:57 -0300

    Adicionado uma classe de excessão para cobrir erros 400

---
+++ src/main/java/br/com/project/register/exceptions/Compiled400Exception.java
@@ -0,0 +1,9 @@
+package br.com.project.register.exceptions;
+
+public class Compiled400Exception extends RuntimeException{
+
+    public Compiled400Exception(String msg){
+        super(msg);
+    }
+
+}
