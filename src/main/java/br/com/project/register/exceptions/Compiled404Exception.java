f9cb89855ff532c837ee120e7153b61728f5765d
Cristie Ferreira <cristie_lopes_ferreira@hotmail.com>
2022-07-14 14:53:14 -0300

    Trocado o nome da classe

---
+++ src/main/java/br/com/project/register/exceptions/Compiled404Exception.java
@@ -0,0 +1,9 @@
+package br.com.project.register.exceptions;
+
+public class Compiled404Exception extends RuntimeException{
+
+    public Compiled404Exception(String msg){
+        super(msg);
+    }
+
+}
