10fb75677bd2268d961fe61d3cbf5268dd33247f
Cristie Ferreira <cristie_lopes_ferreira@hotmail.com>
2022-07-28 10:31:24 -0300

    Adicionado um profile na classe teste, remoção do public nos métodos e adicionado um método de teste

--- src/test/java/br/com/project/register/services/IntegrationTests/CustomerServiceIntegrationTesting.java
+++ src/test/java/br/com/project/register/services/IntegrationTests/CustomerServiceIntegrationTesting.java
@@ -1,5 +1,8 @@
 package br.com.project.register.services.IntegrationTests;
 
+import br.com.project.register.entities.Address;
+import br.com.project.register.entities.Customer;
+import br.com.project.register.enums.CustomerTypes;
 import br.com.project.register.factorys.CustomerFactory;
 import com.google.gson.Gson;
 import io.restassured.RestAssured;
@@ -8,20 +11,36 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
+@Profile("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CustomerServiceIntegrationTesting {
+
+    @LocalServerPort
+    int port;
 
     final static Gson gson = new Gson();
 
     @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "http://localhost:8080";
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+
+        defaultCustomer();
     }
 
     @Test
-    public void postRequest() {
+    void postRequest() {
         Response response = given()
                 .header("Content-Type", "application/json")
                 .and()
@@ -35,28 +54,13 @@ public class CustomerServiceIntegrationTesting {
 
     }
 
-    @Test
-    public void postRequestWithSameCPF() {
-        Response response = given()
-                .header("Content-Type", "application/json")
-                .and()
-                .body(gson.toJson(CustomerFactory.defaultCustomer()))
-                .when()
-                .post("/customers")
-                .then()
-                .extract().response();
-
-        Assertions.assertEquals(400, response.statusCode());
-        Assertions.assertEquals("Document already registered!", response.jsonPath().getString("message"));
-
-    }
 
     @Test
-    public void postRequestWithoutAddress() {
+    void postRequestWithoutAddress() {
         Response response = given()
                 .header("Content-type", "application/json")
                 .and()
-                .body(gson.toJson(CustomerFactory.defaultCustomerWithoutAddress()))
+                .body(gson.toJson(CustomerFactory.customerWithoutAddress()))
                 .when()
                 .post("/customers")
                 .then()
@@ -66,11 +70,11 @@ public class CustomerServiceIntegrationTesting {
 
     }
     @Test
-    public void patchRequest() {
+    void patchRequest() {
         Response response = given()
                 .header("Content-type", "application/json")
                 .and()
-                .body(gson.toJson(CustomerFactory.defaultCustomerPatch()))
+                .body(gson.toJson(CustomerFactory.customerPatch()))
                 .when()
                 .patch("/customers/1")
                 .then()
@@ -78,15 +82,14 @@ public class CustomerServiceIntegrationTesting {
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertEquals("José Maria Santos", response.jsonPath().getString("name"));
-
     }
 
     @Test
-    public void patchRequestInvalidId() {
+    void patchRequestInvalidId() {
         Response response = given()
                 .header("Content-type", "application/json")
                 .and()
-                .body(gson.toJson(CustomerFactory.defaultCustomerPatch()))
+                .body(gson.toJson(CustomerFactory.customerPatch()))
                 .when()
                 .patch("/customers/5")
                 .then()
@@ -97,7 +100,7 @@ public class CustomerServiceIntegrationTesting {
     }
 
     @Test
-    public void getRequest() {
+    void getRequest() {
         Response response = given()
                 .contentType(ContentType.JSON)
                 .when()
@@ -109,24 +112,26 @@ public class CustomerServiceIntegrationTesting {
     }
 
     @Test
-    public void getIdRequest() {
+    void getIdRequest() {
+
         Response response = given()
                 .contentType(ContentType.JSON)
-                .pathParam("id", "1")
+                .pathParam("id", 1)
                 .when()
                 .get("/customers/{id}")
                 .then()
                 .extract().response();
 
+        System.out.println(response.jsonPath().prettify());
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertEquals("joao@email.com", response.jsonPath().getString("email"));
     }
 
     @Test
-    public void getIdInvalidRequest() {
+    void getIdInvalidRequest() {
         Response response = given()
                 .contentType(ContentType.JSON)
-                .pathParam("id", "2")
+                .pathParam("id", "5")
                 .when()
                 .get("/customers/{id}")
                 .then()
@@ -138,7 +143,7 @@ public class CustomerServiceIntegrationTesting {
 
 
     @Test
-    public void deleteRequest() {
+    void deleteRequest() {
         Response response = given()
                 .header("Content-type", "application/json")
                 .when()
@@ -150,15 +155,31 @@ public class CustomerServiceIntegrationTesting {
     }
 
     @Test
-    public void deleteIdInvalidRequest() {
+    void deleteIdInvalidRequest() {
         Response response = given()
                 .header("Content-type", "application/json")
                 .when()
-                .delete("/customers/2")
+                .delete("/customers/5")
                 .then()
                 .extract().response();
 
         Assertions.assertEquals(404, response.statusCode());
+    }
+
+   Response defaultCustomer(){
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new Address("José Antônio","223", "Vila Nova", "Bananeira", "12345678","São Paulo" , true));
+        Customer customer = new Customer("João Cesaro", "592.640.690-03", "joao@email.com", "12912342345", CustomerTypes.valueOf("PF"), addresses);
+
+          Response response = given()
+                .header("Content-Type", "application/json")
+                .and()
+                .body(gson.toJson(customer))
+                .when()
+                .post("/customers")
+                .then()
+                .extract().response();
+        return  response;
     }
 
 }
